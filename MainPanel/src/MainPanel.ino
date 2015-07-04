7ab80bbaccab6ca7a275ac4c16333fe7ba750295
Daniel Loman <dloman@toyon.com>
2015-07-04 13:49:33 -0700

    added adafruit sample code to get led ring working

--- MainPanel/src/MainPanel.ino
+++ MainPanel/src/MainPanel.ino
@@ -25,6 +25,21 @@ void serialSwitchStates();
 
 Panel panel;
 
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h> // Comment out this line for non-AVR boards (Arduino Due, etc.)
+
+#define PIN 6
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(24, 16, NEO_GRB + NEO_KHZ800);
+
+
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 void setupSwitchLed(int Switch, int Led)
@@ -52,6 +67,19 @@ void setup()
   }
   setupSwitchLed(panel.keySwitch, panel.keyLed);
   setupSwitchLed(panel.redSwitch, panel.redLed);
+  strip.begin();
+  strip.show();
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
 }
 
 //------------------------------------------------------------------------------
@@ -142,7 +170,10 @@ void offMode()
 //------------------------------------------------------------------------------
 void loop()
 {
+  colorWipe(strip.Color(255, 0, 0), 50); // Red
   getSwitchStates();
+  colorWipe(strip.Color(0, 255, 0), 50); // Green
   writeLeds();
+  colorWipe(strip.Color(0, 0, 255), 50); // Blue
   readAndWriteSiren();
 }
