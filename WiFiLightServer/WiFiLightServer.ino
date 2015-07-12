f5a3df6bc4199195d65b08c45bc10b3762c61ee7
Daniel Loman <dloman@toyon.com>
2015-07-12 14:40:28 -0700

    added WifiLigth server and rgb esp code

---
+++ WiFiLightServer/WiFiLightServer.ino
@@ -0,0 +1,122 @@
+/*
+ *  This sketch demonstrates how to set up a simple HTTP-like server.
+ *  The server will set a GPIO pin depending on the request
+ *    http://server_ip/gpio/0 will set the GPIO2 low,
+ *    http://server_ip/gpio/1 will set the GPIO2 high
+ *  server_ip is the IP address of the ESP8266 module, will be
+ *  printed to Serial when the module is connected.
+ */
+
+#include <ESP8266WiFi.h>
+#include "WifiNetworkData.h" //ssid name and wifi password
+
+void printResponse(WiFiClient c, int value);
+
+void printResponse(WiFiClient c, int value){
+    // Prepare the response
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/json\r\n\r\n{\"lights\":";
+  s += (value)?"\"1":"\"0";
+  s += "\"}";
+
+  // Send the response to the client
+  c.print(s);
+  delay(1);
+}
+
+
+// Create an instance of the server
+// specify the port to listen on as an argument
+WiFiServer server(80);
+
+int wifiDownCount = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // prepare GPIO2
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+  WiFi.mode(WIFI_AP);
+  connectToWifiNetwork();
+}
+
+int val;
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    connectToWifiNetwork();
+  }
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+
+  // Match the request
+  if (req.indexOf("/off") != -1){
+    val = 0;
+    printResponse(client, val);
+    digitalWrite(2, val);
+  }
+  else if (req.indexOf("/on") != -1){
+    val = 1;
+    printResponse(client, val);
+    digitalWrite(2, val);
+  }
+  else if (req.indexOf("/status") != -1){
+    printResponse(client, val);
+  }
+  else if(req.indexOf("/help") != -1){
+    client.print("Available commands:\r\non\r\noff\r\nstatus");
+    delay(1);
+  }
+  else {
+    Serial.println("invalid request");
+    client.stop();
+    return;
+  }
+
+  client.flush();
+
+  Serial.println("Client disonnected");
+  // The client will actually be disconnected
+  // when the function returns and 'client' object is detroyed
+}
+
+void connectToWifiNetwork()
+{
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+}
---
+++ rgbLeds/rgbLeds.ino
@@ -0,0 +1,103 @@
+#include "Adafruit_WS2801.h"
+#include "SPI.h"
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include "WifiNetworkData.h" //ssid name and wifi password
+
+// Data pin that led data will be written out over
+
+// Clock pin only needed for SPI based chipsets when not using hardware SPI
+char packetBuffer[2048]; //buffer to hold incoming packet,
+
+WiFiUDP Udp;
+const uint8_t dataPin  = 2;    // Yellow wire on Adafruit Pixels
+const uint8_t clockPin = 0;
+Adafruit_WS2801 strip = Adafruit_WS2801(381, dataPin, clockPin);
+
+int timeSinceLastPacket;
+const int timeOut = 5000;
+
+
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(0, OUTPUT);
+  Serial.begin(115200);
+  delay(10);
+
+  timeSinceLastPacket = 0;
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+  Udp.begin(31337);
+    for(int i = 0; i < strip.numPixels(); i++)
+    {
+      strip.setPixelColor(i, Color(0, 0, 0));
+    }
+    strip.show();
+}
+
+void loop()
+{
+  int packetSize = Udp.parsePacket();
+  Udp.read(packetBuffer,2048);
+  if (packetSize >= 460)
+  {
+    //Serial.print('[');
+    for(int i = 0; i < strip.numPixels(); i++)
+    {
+      strip.setPixelColor(
+        i,
+        Color(
+          (*(packetBuffer + (i*3))),
+          (*(packetBuffer + ((i*3) + 1))),
+          (*(packetBuffer + ((i*3) + 2)))));
+      //Serial.print('(');
+      //Serial.print((*(packetBuffer + (i*3))),DEC);
+      //Serial.print(",");
+      //Serial.print((*(packetBuffer + ((i*3) + 1))), DEC);
+      //Serial.print(",");
+      //Serial.print((*(packetBuffer + ((i*3) + 2))),DEC);
+      //Serial.print(") ");
+    }
+    //Serial.println("]");
+    strip.show();
+    timeSinceLastPacket = millis();
+  }
+  if (millis() - timeSinceLastPacket > timeOut)
+  {
+    for(int i = 0; i < strip.numPixels(); i++)
+    {
+
+      strip.setPixelColor(i, Color(0, 0, 0));
+    }
+    strip.show();
+  }
+}
+
+// Create a 24 bit color value from R,G,B
+uint32_t Color(byte r, byte g, byte b)
+{
+  uint32_t c;
+  c = r;
+  c <<= 8;
+  c |= g;
+  c <<= 8;
+  c |= b;
+  return c;
+}
